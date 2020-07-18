67fdb373355f067f29d2d39224ad7a4743550a77
daojie <wangzihao@51dcool.com>
2020-07-18 10:12:21 +0800

    insert controller

---
+++ src/main/java/com/daojie/git/user/UserController.java
@@ -0,0 +1,12 @@
+package com.daojie.git.user;
+
+/**
+ * @author dancinghorse
+ * @version v0.0.1
+ * @date 2020/7/18 - 上午10:09
+ */
+public class UserController {
+	public static void main(String[] args) {
+		System.out.println("hello git idea");
+	}
+}
